0b1e00533c6b9aa6be928706ef451f18fefc2399
OsheaReid <oreid10@my.centennialcollege.ca>
2024-03-25 12:04:44 -0400

    Add project files.

---
+++ ICET4COMP367/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ICET4COMP367.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ICET4COMP367.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
